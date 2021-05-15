fbb46e1569f19c4e1028df24a7d3d3c1966332e2
Lovro Habjan <lh5107@student.uni-lj.si>
2021-05-15 10:28:48 +0200

    Cosmetic fixes
    Formatted tex file to 80 column.
    
    Added missing newlines and identation to MATLAB files.

--- src/demo.m
+++ src/demo.m
@@ -24,28 +24,28 @@ runTest(b5, 5);
 function runTest(b, testNum)
 % runTest - auxiliary function
 % Computes points for all t = [0, 0.01, ..., 1] and plots the difference
-ts = [] * 100;
-diffsInfy = [] * 100;
-diffsSquare = [] * 100;
-diffsMax = [] * 100;
-t = 0.0;
-for i = 1 : 100
-    [v1, v2] = bezier2d(b, t);
-    diffsMax(i) = max(abs(v1 - v2));
-    diffsInfy(i) = sum(abs(v1 - v2));
-    diffsSquare(i) = sqrt(sum(v1 - v2)^2);
-    ts(i) = t;
-    t = t + 0.01;
-end
+    ts = [] * 100;
+    diffsInfy = [] * 100;
+    diffsSquare = [] * 100;
+    diffsMax = [] * 100;
+    t = 0.0;
+    for i = 1 : 100
+        [v1, v2] = bezier2d(b, t);
+        diffsMax(i) = max(abs(v1 - v2));
+        diffsInfy(i) = sum(abs(v1 - v2));
+        diffsSquare(i) = sqrt(sum(v1 - v2)^2);
+        ts(i) = t;
+        t = t + 0.01;
+    end
 
-figure(testNum)
-clf
-hold on
-plot(ts, diffsInfy, 'b', 'DisplayName', 'infy')
-plot(ts, diffsMax, 'g', 'DisplayName', 'max')
-plot(ts, diffsSquare, 'r', 'DisplayName', 'square')
-xlabel("Value of t");
-ylabel("Difference");
-title("Comparison of algorithms for test " + testNum);
-legend;
+    figure(testNum)
+    clf
+    hold on
+    plot(ts, diffsInfy, 'b', 'DisplayName', 'infy')
+    plot(ts, diffsMax, 'g', 'DisplayName', 'max')
+    plot(ts, diffsSquare, 'r', 'DisplayName', 'square')
+    xlabel("Value of t");
+    ylabel("Difference");
+    title("Comparison of algorithms for test " + testNum);
+    legend;
 end
